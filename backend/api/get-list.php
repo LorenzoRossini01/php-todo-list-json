f7bb3106dcafb879632cc1d12e494f9f80ab0e45
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-02-29 17:24:14 +0100

    feat: get data from json to php

--- backend/api/get-list.php
+++ backend/api/get-list.php
@@ -1,1 +1,10 @@
 <?php
+header("Content-Type: application/json");
+
+$json_task_list = json_decode(file_get_contents("../data/todolist.json"), true);
+
+// var_dump($json_task_list);
+
+$todo_array=json_encode($json_task_list);
+
+echo $todo_array;
