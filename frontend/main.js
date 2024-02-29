a3be1eb3891653972afa6e3133eca3cfaefd9762
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-02-29 17:11:48 +0100

    feat: general structure html, js

--- frontend/main.js
+++ frontend/main.js
@@ -4,6 +4,13 @@ const app = createApp({
   data() {
     return {
       title: "Persistent todo list",
+      todoList: [
+        { name: "Task 1", done: false },
+        { name: "Task 2", done: false },
+        { name: "Task 3", done: false },
+        { name: "Task 4", done: false },
+        { name: "Task 5", done: false },
+      ],
     };
   },
 });
