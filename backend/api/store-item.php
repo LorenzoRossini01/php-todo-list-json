a104465cee3b9df4842aedcc00e253b98241c7cd
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-03-01 12:50:10 +0100

    fix: add task

--- backend/api/store-item.php
+++ backend/api/store-item.php
@@ -5,6 +5,8 @@ $new_task=[
     'done'=> $_POST['done']==='true',
 ];
 
+// var_dump($_POST);
+
 // var_dump( $new_task );
 
 $json_todolist=json_decode(file_get_contents('../data/todolist.json'),true);
@@ -15,8 +17,8 @@ $json_todolist[] = $new_task;
 
 header('Content-Type: application/json');
 
-$todo_array=json_encode($json_task_list);
+$todo_array=json_encode($json_todolist);
 
-// file_put_contents('../data/todolist.json', $todo_array);
+file_put_contents('../data/todolist.json', $todo_array);
 
 echo $todo_array;
