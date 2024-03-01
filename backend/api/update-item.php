5bfd47b508b6ee75d4b9ae830338e2f178099263
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-03-01 12:50:31 +0100

    feat: update task state

--- backend/api/update-item.php
+++ backend/api/update-item.php
@@ -1,22 +1,24 @@
 <?php
 
-header('Content-Type: application/json');
-
 $updated_task=[
     'name'=> $_POST['name'],
     'done'=> $_POST['done']==='true',
 ];
 $to_update_index= (int)$_POST['index'];
+
+// var_dump($_POST);
+
 // var_dump( $new_task );
 
 $json_todolist=json_decode(file_get_contents('../data/todolist.json'),true);
 
 $json_todolist[$to_update_index] = $updated_task;
-
 // // // var_dump($json_todolist);
 
-$todo_array=json_encode($json_task_list);
+header('Content-Type: application/json');
 
-// file_put_contents('../data/todolist.json', $todo_array);
+$todo_array=json_encode($json_todolist);
+
+file_put_contents('../data/todolist.json', $todo_array);
 
 echo $todo_array;
